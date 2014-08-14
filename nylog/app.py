af86285162cc4858d53f0fee38073cbf7ecefa92
Florent Thévenet <florent@fthevenet.fr>
2014-08-15 01:48:22 +0200

    Show list of recent posts and display a single post

--- nylog/app.py
+++ nylog/app.py
@@ -1,7 +1,36 @@
 from flask import Flask, render_template
+import arrow
 
 app = Flask('nylog')
 
 @app.errorhandler(404)
 def handler_page_not_found(e):
     return render_template("404.html"), 404
+
+@app.template_filter()
+def format_date_local(date):
+    "Human friendly datetime display"
+    adate = arrow.get(date)
+    locale = 'en_us'
+
+    diff = arrow.utcnow() - adate    
+    if diff.days <= 2:
+        return adate.humanize(locale = locale)
+    else:
+        return adate.format('DD MMMM YYYY, HH:mm', locale = locale)
+
+# from http://flask.pocoo.org/snippets/28/
+import re
+from jinja2 import evalcontextfilter, Markup, escape
+
+_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
+
+@app.template_filter()
+@evalcontextfilter
+def paragraphs(eval_ctx, value):
+    "Replace newlines by html <br/> tags and divides the text into <p>(aragraphs)"
+    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
+        for p in _paragraph_re.split(escape(value)))
+    if eval_ctx.autoescape:
+        result = Markup(result)
+    return result
--- nylog/log.py
+++ nylog/log.py
@@ -1,10 +1,24 @@
 from flask import render_template
 from .app import app
-from .models import db, User, Post, Comment
+from .models import db, User, Post, Comment, Category
 
 @app.route('/')
 def index():
-    return render_template("home.html")
+    posts = Post.query.limit(5)#join(Post.categories).\
+            #join(Category.users).\
+            #filter()
+    """SELECT * FROM post p JOIN posts_categories pc ON ...
+    JOIN users_categories uc ON ...
+    WHERE (uc.user IS NULL) OR (uc.user == $user)
+    """
+    return render_template("home.html", posts = posts)
+
+
+@app.route('/posts')
+def old_posts():
+    posts = Post.query.paginate(page, 5, error_out = False)
+    return render_template("old.html", posts = posts)
+
 
 @app.route('/post/<int:id>/<slug>')
 def show_post(id,  slug):
