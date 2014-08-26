eea55640dd8d6c86e09a7768314c730493f8d866
Florent Thévenet <florent@fthevenet.fr>
2014-08-25 23:25:10 -0400

    Filter posts by user

--- nylog/log.py
+++ nylog/log.py
@@ -1,16 +1,23 @@
 from flask import render_template
+from flask.ext.login import current_user
 from .app import app
-from .models import db, User, Post, Comment, Category
+from .models import db, User, Post, Comment, Category, users_categories
+from sqlalchemy.sql.expression import literal
 
 @app.route('/')
 def index():
-    posts = Post.query.limit(5)#join(Post.categories).\
-            #join(Category.users).\
-            #filter()
-    """SELECT * FROM post p JOIN posts_categories pc ON ...
-    JOIN users_categories uc ON ...
-    WHERE (uc.user IS NULL) OR (uc.user == $user)
-    """
+    if not(current_user.is_anonymous()) and current_user.is_authenticated():
+        if current_user.is_admin:
+            # He can see everything
+            query = Post.query
+        else:
+            # They can see public posts and posts in their categories
+            query = Post.query.join(Post.categories).filter((Category.public == True) | Category.allowed_users.any(id = current_user.id))
+    else:
+        # They can only see public posts
+        query = Post.query.join(Post.categories).filter(Category.public == True)
+
+    posts = query.group_by(Post.id).order_by(Post.covered_period).limit(5)
     return render_template("home.html", posts = posts)
 
 
